0ad70d6e4eae4d16a7994705264c477777e3b3c7
Jonah Stiennon <jonahss@gmail.com>
2015-04-29 11:56:15 -0700

    simplify npm module. don't try to compile or bundle built app

--- build.sh
+++
@@ -1,6 +0,0 @@
-LASTDIR=`pwd`
-rm -rf ./build
-xcodebuild -project SafariLauncher.xcodeproj -configuration Debug SYMROOT=build
-cd ./build/Debug-iphoneos/
-zip -r SafariLauncher.zip SafariLauncher.app
-cd $LASTDIR
